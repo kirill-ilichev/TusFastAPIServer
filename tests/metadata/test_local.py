2cff4d092037fd32dbc7f5b6c233c8106849fefd
kirill-ilichev <ilichev.kirill.s@gmail.com>
2025-01-22 10:12:49 +0400

    Finalize tests for local metadata

--- tests/metadata/test_local.py
+++ tests/metadata/test_local.py
@@ -2,12 +2,14 @@ import pytest
 import os
 import tempfile
 import json
+from unittest import mock
 from freezegun import freeze_time
 
 from tusfastapiserver.config import Config
 from tusfastapiserver.config import MetadataStrategyType
 from tusfastapiserver.config import StorageStrategyType
 from tusfastapiserver.metadata import LocalMetadataStrategy
+from tusfastapiserver.schemas import UploadMetadata
 
 
 @pytest.fixture(scope="class")
@@ -88,3 +90,96 @@ class TestLocalMetadataStrategy:
             local_metadata_strategy.config.metadata_path = temp_dir
             local_metadata_strategy._check_or_make_folder(temp_dir)
             assert os.path.exists(temp_dir)
+
+    def test_create_metadata_file(self, local_metadata_strategy):
+        with tempfile.TemporaryDirectory() as temp_dir:
+            file_id = "123"
+            local_metadata_strategy.config.metadata_path = temp_dir
+            metadata_path = local_metadata_strategy.generate_metadata_path(file_id)
+            os.makedirs(os.path.dirname(metadata_path), exist_ok=True)
+            metadata = UploadMetadata(
+                id=file_id,
+                upload_storage_path="test",
+                upload_metadata_path=metadata_path,
+                storage_strategy_type=StorageStrategyType.LOCAL,
+                metadata_strategy_type=MetadataStrategyType.LOCAL,
+            )
+            local_metadata_strategy._create_metadata_file(metadata)
+            assert os.path.exists(metadata_path)
+            with open(metadata_path, "r") as f:
+                metadata_from_file = UploadMetadata(**json.load(f))
+                assert metadata_from_file.id == file_id
+                assert metadata_from_file.upload_storage_path == "test"
+                assert metadata_from_file.upload_metadata_path == metadata_path
+                assert metadata_from_file.upload_offset == 0
+                assert metadata_from_file.upload_length == None
+                assert metadata_from_file.upload_defer_length == None
+                assert metadata_from_file.metadata == None
+
+    def test_initialize(self, local_metadata_strategy):
+        upload_metadata = UploadMetadata(
+            id="123",
+            upload_storage_path="test",
+            upload_metadata_path="test",
+            storage_strategy_type=StorageStrategyType.LOCAL,
+            metadata_strategy_type=MetadataStrategyType.LOCAL,
+        )
+        with mock.patch.object(
+            local_metadata_strategy, "_check_or_make_folder"
+        ) as mock_check_or_make_folder, mock.patch.object(
+            local_metadata_strategy, "_create_metadata_file"
+        ) as mock_create_metadata_file:
+            local_metadata_strategy.initialize(upload_metadata)
+            mock_check_or_make_folder.assert_called_once_with(
+                upload_metadata.upload_metadata_path
+            )
+            mock_create_metadata_file.assert_called_once_with(upload_metadata)
+
+    def test_update_metadata_file(self, local_metadata_strategy):
+        file_id = "123"
+        with tempfile.TemporaryDirectory() as temp_dir:
+            local_metadata_strategy.config.metadata_path = temp_dir
+            metadata_path = local_metadata_strategy.generate_metadata_path(file_id)
+            os.makedirs(os.path.dirname(metadata_path), exist_ok=True)
+            initial_metadata = UploadMetadata(
+                id=file_id,
+                upload_storage_path="test",
+                upload_metadata_path=metadata_path,
+                storage_strategy_type=StorageStrategyType.LOCAL,
+                metadata_strategy_type=MetadataStrategyType.LOCAL,
+            )
+            local_metadata_strategy._create_metadata_file(initial_metadata)
+            updated_metadata = UploadMetadata(
+                id=file_id,
+                upload_storage_path="test_updated",
+                upload_metadata_path=metadata_path,
+                storage_strategy_type=StorageStrategyType.LOCAL,
+                metadata_strategy_type=MetadataStrategyType.LOCAL,
+                upload_offset=100,
+                upload_length=200,
+                metadata={"key": "value"},
+            )
+            local_metadata_strategy._update_metadata_file(updated_metadata)
+            with open(metadata_path, "r") as f:
+                metadata_from_file = UploadMetadata(**json.load(f))
+                assert metadata_from_file.id == file_id
+                assert metadata_from_file.upload_storage_path == "test_updated"
+                assert metadata_from_file.upload_metadata_path == metadata_path
+                assert metadata_from_file.upload_offset == 100
+                assert metadata_from_file.upload_length == 200
+                assert metadata_from_file.upload_defer_length == None
+                assert metadata_from_file.metadata == {"key": "value"}
+
+    def test_update(self, local_metadata_strategy):
+        upload_metadata = UploadMetadata(
+            id="123",
+            upload_storage_path="test",
+            upload_metadata_path="test",
+            storage_strategy_type=StorageStrategyType.LOCAL,
+            metadata_strategy_type=MetadataStrategyType.LOCAL,
+        )
+        with mock.patch.object(
+            local_metadata_strategy, "_update_metadata_file"
+        ) as mock_update_metadata_file:
+            local_metadata_strategy.update(upload_metadata)
+            mock_update_metadata_file.assert_called_once_with(upload_metadata)
