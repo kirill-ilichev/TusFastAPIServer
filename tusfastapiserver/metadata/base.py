18119f0b7eb79a01ec86306fbadedd6c138358c6
kirill-ilichev <ilichev.kirill.s@gmail.com>
2025-01-24 18:04:16 +0400

    Improve interface for base metadata

--- tusfastapiserver/metadata/base.py
+++ tusfastapiserver/metadata/base.py
@@ -1,5 +1,6 @@
 from tusfastapiserver.config import Config
 from tusfastapiserver.config import MetadataStrategyType
+from tusfastapiserver.schemas import UploadMetadata
 
 
 class BaseMetadataStrategy:
@@ -12,4 +13,10 @@ class BaseMetadataStrategy:
         raise NotImplementedError()
 
     def is_metadata_exists(self, file_id: str) -> bool:
+        raise NotImplementedError()
+
+    def get_metadata(self, file_id: str) -> UploadMetadata:
+        raise NotImplementedError()
+
+    def update(self, upload_metadata: UploadMetadata, *args, **kwargs):
         raise NotImplementedError()
